0ad9e4a136c3b8214eb9e6b899bf2efd71dda9fc
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-12-15 20:42:06 -0300

    implementação da página de notícia

--- single-blog.php
+++ single-blog.php
@@ -8,9 +8,32 @@
 
     <div class="cover" style="background-image: url(<?= $url_cover ?>);">
       <div class="overlay">
-        <span>Publicado em 01/01/2021</span>
+        <span>Publicado em <?php the_date(); ?></span>
         <?php the_title('<h1>', '</h1>'); ?>
-        <a href="">Nome da categoria</a>
+        <?php the_category(); ?>
+      </div>
+    </div>
+
+    <div class="content">
+      <?php the_content(); ?>
+
+      <footer>
+        <p>
+          <?php echo get_avatar(the_author_meta('ID', 60)); ?>
+        </p>
+        <p>
+          <?php the_author_meta('description'); ?>
+        </p>
+      </footer>
+
+      <aside>
+        <?php the_tags('', ' ', ''); ?>
+      </aside>
+
+      <div class="share">
+        <a href="" title="Voltar"><i class="fas fa-long-arrow-alt-left"></i></a>
+        <a href="" title="Compartilhar via Facebook"><i class="fab fa-facebook-f"></i></a>
+        <a href="" title="Compartilhar via Linkedin"><i class="fab fa-linkedin-in"></i></a>
       </div>
     </div>
   </main>
